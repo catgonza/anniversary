10f85fa5c5f08614928d8b311134fe2fa0c29e39
Catalina <67137308+catgonza@users.noreply.github.com>
2021-10-18 21:47:18 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+function displayRandomImages()   
+{  
+   //array of images with image location, height, and width  
+   var imageArray = [  
+   {   
+     //address URL of the image  
+     src: "4.png",  
+     //size for the image to be display on webpage  
+     width: "280",  
+     height: "200"  
+   },   
+   {  
+     src: "2.png",  
+     width: "320",  
+     height: "195"  
+   },   
+   {  
+     src: "3.png",  
+     width: "320",  
+     height: "195"  
+   },  
+   {  
+     src: "1.png",  
+     width: "350",  
+     height: "250"  
+    } ];  
+      
+    //find the length of the array of images  
+    var arrayLength = imageArray.length;  
+    var newArray = [];  
+    for (var i = 0; i < arrayLength; i++) {  
+        newArray[i] = new Image();  
+        newArray[i].src = imageArray[i].src;  
+        newArray[i].width = imageArray[i].width;  
+        newArray[i].height = imageArray[i].height;  
+    }  
+     
+  // create random image number  
+  function getRandomNum(min, max)   
+  {  
+      // generate and return a random number for the image to be displayed   
+      imgNo = Math.floor(Math.random() * (max - min + 1)) + min;  
+      return newArray[imgNo];  
+  }    
+  
+  // 0 is first image and (preBuffer.length - 1) is last image of the array  
+  var newImage = getRandomNum(0, newArray.length - 1);  
+   
+  // remove the previous images  
+  var images = document.getElementsByTagName('img');  
+  var l = images.length;  
+  for (var p = 0; p < l; p++) {  
+     images[0].parentNode.removeChild(images[0]);  
+  }  
+  // display the new random image    
+  document.body.appendChild(newImage);  
+}  
